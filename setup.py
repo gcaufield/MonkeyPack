ebf826dae952b7454f4fbd96206bf32589b14800
Greg Caufield <greg.caufield@gmail.com>
2020-10-08 20:57:27 -0600

    Bump version: 0.2.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
     ]
 
 setup(name='mbpkg',
-      version='0.2.0',
+      version='1.0.0',
       description='Connect IQ Package Manager',
       long_description=readme,
       long_description_content_type='text/markdown',
