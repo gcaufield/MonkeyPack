61a3db6972fc60f44b667607411c62fb78d168f3
Greg Caufield <greg@embeddedcoffee.ca>
2020-10-11 20:22:42 -0600

    Cover missing branch in Version tests
    Missed the does not match case in the initial set of tests.

--- tests/test_version.py
+++ tests/test_version.py
@@ -7,16 +7,20 @@ class TestVersion(unittest.TestCase):
 
     def test_version_matches(self):
         ver = Version("0.2.4")
-        self.assertEqual(True, ver.matches("0.2.4"))
+        self.assertTrue(ver.matches("0.2.4"))
 
     def test_version_matches_v(self):
         ver = Version("0.2.4")
-        self.assertEqual(True, ver.matches("v0.2.4"))
+        self.assertTrue(ver.matches("v0.2.4"))
+
+    def test_version_doesnt_match(self):
+        ver = Version("0.4.7")
+        self.assertFalse(ver.matches("v0.2.4"))
 
     def test_version_matches_version(self):
         ver = Version("0.4.5")
         ver2 = Version("0.4.5")
-        self.assertEqual(True, ver.matches(ver2))
+        self.assertTrue(ver.matches(ver2))
 
     def test_version_str_returns_ver(self):
         ver = Version("0.4.5")
