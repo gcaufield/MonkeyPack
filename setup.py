a55056d25041ca35f909e9aaf0c3dfe31ae5dfb9
Greg Caufield <greg@embeddedcoffee.ca>
2020-10-12 16:40:27 -0600

    Bump version: 1.1.0 to 1.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = ["requests>=2.0,<3", "PyGithub~=1.53"]
 
 setup(
     name="mbpkg",
-    version="1.1.0",
+    version="1.2.0",
     description="Connect IQ Package Manager",
     long_description=readme,
     long_description_content_type="text/markdown",
