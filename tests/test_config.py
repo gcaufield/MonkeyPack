gcaufield/MonkeyPack

commit a3db4405a25aaa1a3e3d140f3f862c0943c7e9bc

Allow for config to be passed on Command Line
Added `--config` argument to specify a custom path for the config file.

Closes #28

tests/test_config.py
@@ -41,7 +41,7 @@ class MicroMock(object):
 class TestConfig(unittest.TestCase):
     @staticmethod
     def __build_args(
-        token=None, package=None, directory=None, jungle=None, manifest=None
+        token=None, package=None, directory=None, jungle=None, manifest=None, config=None
     ):
         return MicroMock(
             token=token,
@@ -49,6 +49,7 @@ class TestConfig(unittest.TestCase):
             directory=directory,
             jungle=jungle,
             manifest=manifest,
+            config=config
         )
 
     @staticmethod
@@ -79,13 +80,21 @@ class TestConfig(unittest.TestCase):
     def test_can_init_config(self):
         Config(self.__build_args())
 
-    def test_reads_config_file_if_exists(self):
+    def test_reads_default_config_file_if_exists(self):
         self.__mock_path.return_value = True
         m = mock_open(read_data="")
         with patch("builtins.open", m):
             Config(self.__build_args())
 
         m.assert_called_once_with("mbgetcfg.ini", "r")
+
+    def test_reads_config_file_from_args(self):
+        self.__mock_path.return_value = True
+        m = mock_open(read_data="")
+        with patch("builtins.open", m):
+            Config(self.__build_args(config="test.ini"))
+
+        m.assert_called_once_with("test.ini", "r")
 
     def test_token_is_none(self):
         cfg = Config(self.__build_args(token=None))

mbget/config.py
@@ -44,9 +44,14 @@ class Config(object):
     def __init__(self, args):
         self.__args = args
         self.__config_file = None
-        if os.path.exists("mbgetcfg.ini"):
+        if args.config is not None:
+            config_path = args.config
+        else:
+            config_path = "mbgetcfg.ini"
+
+        if os.path.exists(config_path):
             config = ConfigParser()
-            with open("mbgetcfg.ini", "r") as f:
+            with open(config_path, "r") as f:
                 data = f.read()
 
             config.read_string(data)

mbget/main.py
@@ -57,6 +57,7 @@ def run_update(args):
 def main():
     parser = argparse.ArgumentParser(description="Connect IQ Package Manager")
     parser.add_argument("-t", "--token", help="Github API token for requests")
+    parser.add_argument("-c", "--config", help="Specify path to config INI")
     parser.add_argument("-m", "--manifest", help="Specify application manifest")
     parser.add_argument("-p", "--package", help="Specify the package map text file")
     parser.add_argument("-j", "--jungle", help="Barrel Jungle file")

commit 1457f2f143b2ac8f2cccd8c0f2f74cb9e22a3b43

Fix formatting issue
Fix issue introduced in a3db4405a25aaa1a3e3d140f3f862c0943c7e9bc

tests/test_config.py
@@ -41,7 +41,12 @@ class MicroMock(object):
 class TestConfig(unittest.TestCase):
     @staticmethod
     def __build_args(
-        token=None, package=None, directory=None, jungle=None, manifest=None, config=None
+        token=None,
+        package=None,
+        directory=None,
+        jungle=None,
+        manifest=None,
+        config=None,
     ):
         return MicroMock(
             token=token,
@@ -49,7 +54,7 @@ class TestConfig(unittest.TestCase):
             directory=directory,
             jungle=jungle,
             manifest=manifest,
-            config=config
+            config=config,
         )
 
     @staticmethod