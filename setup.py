986b1a0add4dc0a6ec1fb16848dc619ee16af718
Greg Caufield <greg@embeddedcoffee.ca>
2020-11-03 06:52:37 -0700

    Verify Python 3.9 with tox
    Add Python 3.9 to tox configuration, and run it on travis

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="garmin connectiq packages",
     url="https://github.com/gcaufield/MonkeyPack",
